f238e13615a81f8c3e705d0e545ffe83db5ba71b
Eric <eric.talle@hp.com>
2016-04-23 10:30:38 -0700

    Changing over to personal home page.

---
+++ erics_place.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'sinatra/base'
+require 'json'
+require 'yaml'
+require 'date'
+require 'net/http'
+require 'uri'
+require 'haml'
+
+
+class EricsPlace < Sinatra::Base
+  get "/" do
+    "Eric's Place"
+  end
+end
